400ea0b54762637c5e87f9f2d027e9471353e48c
Wilson <wilsonj806@gmail.com>
2019-04-13 14:53:28 -0400

    Integrate mongoose and mongodb, add basic db read
    - Integrate Mongoose into app.js
      - add scripting to connect Mongoose, and MongoDB to the app
        - add error checking to ensure that the app connects properly
      - add scripting to read/ fetch articles from mongodb
      - add scripting to render articles into the homepage
    - Add models/article.js
      - add basic Mongoose schema for application level ORM

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+// init mongoose and database connection
+mongoose.connect('mongodb://localhost:27017/nodkb', {useMongoClient: true});
+let db = mongoose.connection;
+
+// check connection
+db.once('open', function(){
+  console.log('Connected to MongoDB')
+});
+
+// check for DB errors
+db.on('error', function(error){console.error(error)});
 
 // init express, set port
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Load Pug, the View Engine
+// bring in DB Models
+let Article = require('./models/article');
 
+
+// Load Pug, the View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Home route
-
-app.get('/', (req, res) => {
-  let articles = [
-    {
-      id: 1,
-      title: 'Article 1',
-      author: 'brad traversy',
-      body: 'This is article 1'
-    },
-    {
-      id: 2,
-      title: 'Article 2',
-      author: 'wilson',
-      body: 'This is article 2'
-    },
-    {
-      id: 3,
-      title: 'Article 3',
-      author: 'john doe',
-      body: 'This is article 3'
+app.get('/', function(req, res) {
+  // fetch articles, pass the articles into the response for rendering
+  Article.find({}, (error, articles) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.render('index', {
+        "title": 'Hello',
+        "articles": articles
+      });
     }
-  ];
-  res.render('index', {
-    "title": 'Hello',
-    "articles": articles
   });
 });
 
-// Add route
+// Add articles route
 
 app.get('/articles/add', (req, res) => {
   res.render('add_article', {
@@ -47,4 +49,25 @@ app.get('/articles/add', (req, res) => {
   });
 });
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, function() {console.log(`Server started on port ${PORT}`)});
+
+/* let articles = [
+  {
+    id: 1,
+    title: 'Article 1',
+    author: 'brad traversy',
+    body: 'This is article 1'
+  },
+  {
+    id: 2,
+    title: 'Article 2',
+    author: 'wilson',
+    body: 'This is article 2'
+  },
+  {
+    id: 3,
+    title: 'Article 3',
+    author: 'john doe',
+    body: 'This is article 3'
+  }
+]; */
--- docs/notes.md
+++ docs/notes.md
@@ -12,3 +12,29 @@ Below are a list of references to use/ that will be used throughout:
 Additional notes/ repos for reference below:
 
 - [MongoDB Crash Course](./mongodb-crash.md)
+
+## Basic Notes For Packages
+
+### Pug.js
+
+- [Pug.js website](https://pugjs.org/api/getting-started.html)
+
+Pug.js or Jade.js as it was previously known is a templating engine for writing HTML templates in JavaScript. Other commonly used templating engines are Handlebars and EJS.
+
+The syntax is weird in comparison to other templating engines or even JSX since it uses indentation for nesting/ dom modeling in a .pug file. See below for an example:
+```pug
+//- I am a .pug comment
+html
+  head
+    title #{title}
+  body
+    h1 'Hello World'
+```
+
+### Mongoose
+
+- [Mongoose website](https://mongoosejs.com/)
+
+Mongoose is an Object Relational Mapping tool for MongoDB. MongoDB is a type of NoSQL database as covered in the [MongoDB crash course](./mongodb-crash.md) and is very flexible. However, because it's super flexible it doesn't really have structure to how you relate your data to each other.
+
+Mongoose lets you structure your data on a lighter level than SQL and on an application level.
